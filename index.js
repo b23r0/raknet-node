6734cb081da286a799ad4b471ad63ede11ffbaff
b23r0 <b23r0@foxmail.com>
2022-05-08 20:21:37 +0800

    support EventEmitter.

--- index.js
+++ index.js
@@ -14,127 +14,78 @@ try{
 	process.exit(1)
 }
 
+function listen(address , cb) {
+
+	raknet.RaknetServer.bind(address).then((server) => {
+		const doSomething = async () => {
+			for (;;){
+				cb(await server.accept())
+			}
+		}
+		doSomething().then(null)
+	})
+}
+
+function recvcb(client , cb) {
+	client.recv().then((buf) => {
+		cb(buf)
+	})
+}
+
+function connectcb(address , cb){
+	raknet.RaknetClient.connect(address).then((client) =>{
+		cb(client)
+	})
+}
+
 const { EventEmitter } = require('events')
 
 class Client extends EventEmitter {
   constructor (hostname, port, options = {}) {
     super()
-    this.client = new RaknetClient(hostname, port)
-    this.ping = () => this.client.ping()
-    this.connect = () => this.client.connect()
-    this.startListening()
+    connectcb(hostname + ":" + port, (client) => {
+		this.client = client
+		this.startListening()
+	})
+
   }
 
-  // Handle inbound packets and emit events
   startListening () {
-    // var recvC = 0
-    this.client.listen((buffers, address, guid) => {
-      for (const buffer of buffers) {
-        const buf = Buffer.from(buffer) // copy native buffer to js
-        const id = buf[0]
-        // console.log('C -> ', buf, address, guid)
-        try {
-          if (id < MessageID.ID_USER_PACKET_ENUM) { // Internal RakNet messages: we handle & emit
-            if (id == MessageID.ID_UNCONNECTED_PONG) {
-              if (buf.byteLength > 5) {
-                const extra = Buffer.from(buf.slice(5))
-                // console.log('Extra', extra.toString())
-                this.emit('pong', { extra })
-              } else {
-                this.emit('pong', {})
-              }
-            }
-            if (id == MessageID.ID_CONNECTION_REQUEST_ACCEPTED) {
-              this.emit('connect', { address, guid })
-            }
-            if (id == MessageID.ID_CONNECTION_LOST || id == MessageID.ID_DISCONNECTION_NOTIFICATION || id == MessageID.ID_CONNECTION_BANNED || id == MessageID.ID_INCOMPATIBLE_PROTOCOL_VERSION) {
-              this.emit('disconnect', { address, guid, reason: id })
-            }
-          } else { // User messages
-            this.emit('encapsulated', { buffer: buf, address, guid })
-          }
-        } catch (e) {
-          console.debug('While decoding', buf.toString('hex'))
-          console.debug('Client failed to read packet', e)
-          this.emit('error', e, buf)
-        }
-      }
-    })
+	
+	address = this.client.peeraddr()
+	
+	this.emit('connect', { address })
+	
+	for (;;){
+		recvcb(this.client , (buf) => {
+			if (buf == null){
+				this.emit('disconnect', { address })
+				break
+			}
+			
+			this.emit('encapsulated', { buffer: buf, address })
+		})
+	}
   }
 
-  send (message, priority, reliability, orderingChannel = 0, broadcast = false) {
-    // When you Buffer.from/allocUnsafe, it may put your data into a global buffer, we need it in its own buffer
-    if (message instanceof Buffer && message.buffer.byteLength != message.byteLength) message = new Uint8Array(message)
-    const ret = this.client.send(message instanceof ArrayBuffer ? message : message.buffer, priority, reliability, orderingChannel, broadcast)
-    if (ret <= 0) {
-      throw Error(`Failed to send: ${ret}`)
-    }
-  }
-}
-
-function ServerClient (server, address, guid) {
-  const [hostname, port] = address.split('/')
-  this.address = address
-  this.guid = guid
-  this.send = (...args) => server.send(hostname, port, ...args)
-  this.close = (silent) => server.kick(guid, silent)
-
-  this.neuter = () => { // Client is disconnected, no-op to block sending
-    this.send = () => { }
+  send (message) {
+    this.client.send(message)
   }
 }
 
 class Server extends EventEmitter {
   constructor (hostname, port) {
     super()
-    this.server = new RaknetServer(hostname, port)
-    this.close = () => this.server.close()
-    this.connections = new Map()
-  }
-
-  listen () {
-    return this.server.listen(packets => {
-      for (const [buffer, address, guid] of packets) {
-        const buf = Buffer.from(buffer)
-        // console.log('S -> ', buffer, address, guid)
-        try {
-          const id = buf[0]
-          if (id < MessageID.ID_USER_PACKET_ENUM) { // Internal RakNet messages: we handle & emit
-            if (id == MessageID.ID_NEW_INCOMING_CONNECTION) {
-              const client = new ServerClient(this, address, guid)
-              this.connections.set(guid, client)
-              this.emit('openConnection', client)
-            } else if (id == MessageID.ID_DISCONNECTION_NOTIFICATION || id == MessageID.ID_CONNECTION_LOST || id == MessageID.ID_INCOMPATIBLE_PROTOCOL_VERSION) {
-              if (this.connections.has(guid)) {
-                const con = this.connections.get(guid)
-                this.emit('closeConnection', con, id)
-                con.neuter()
-              }
-              this.connections.delete(guid)
-            }
-          } else { // User messages
-            this.emit('encapsulated', { buffer: buf, address, guid })
-          }
-        } catch (e) { // If we don't handle this the program will segfault
-          console.debug('While decoding', buf.toString('hex'))
-          console.debug('Server failed to read packet', e)
-          this.emit('error', e, buf)
-        }
-      }
-    })
-  }
-
-  send (sendAddr, sendPort, message, priority, reliability, orderingChannel = 0, broadcast = false) {
-    if (message instanceof Buffer && message.buffer.byteLength != message.byteLength) message = new Uint8Array(message)
-    // console.warn('SENDING', arguments)
-    const ret = this.server.send(sendAddr, parseInt(sendPort), message instanceof ArrayBuffer ? message : message.buffer, priority, reliability, orderingChannel, broadcast)
-    if (ret <= 0) {
-      throw Error(`Failed to send: ${ret}`)
-    }
-  }
-
-  kick (clientGuid, silent) {
-    this.server.kick(clientGuid, silent)
+    listen(hostname + ":" + port, (client) => {
+		this.emit('openConnection', client)
+		recvcb(client , (buf) => {
+			if (buf == null){
+				this.emit('closeConnection', { client.peeraddr() })
+				break
+			}
+			this.emit('encapsulated', { buffer: buf, client.peeraddr()})
+		})
+	})
   }
 }
 
